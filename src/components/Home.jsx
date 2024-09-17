c6c4a22d82cbff56e5b758a845e49637081f7beb
faith obetta <obettafaith24@gmail.com>
2024-09-17 11:50:10 +0100

    git

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,13 +7,13 @@ const Home = () => {
   return (
     <div className='flex justify-around items-center p-10 space-x-10 lg:flex-row ssm:flex-col ssm:space-y-10 text-white' id='home'>
         <div className='lf:w-1/2 ssm:w-fit'>
-            <img src={me} alt="" className="rounded-full w-60 h-50  border-8 border-white" />
+            <img src={me} alt="" className="rounded-full w-60 h-50 mx-10 border-8 border-white" />
 
-            <p className='text-4xl mx-8 mb-5'> Hi, I'm</p>
-            <h3 className='text-4xl mx-8'>Faith Obetta</h3>
+            <p className='text-4xl mx-16 mb-5'> Hi, I'm</p>
+            <h3 className='text-4xl mx-16'>Faith Obetta</h3>
             <hr />
             <p className='mt-10 text-2xl text-slate-300 font-sans gradiant-text running-text'>A Front-End Developer & UX/UI Designer</p>
-            <div className='flex mt-6 space-x-6 cursor-pointer'>
+            <div className='flex mx-16 mt-6 space-x-6 cursor-pointer'>
                 <a href="https://github.com/faithobetta"><FaGithub size={40} className='border-4 hover:border-indigo-800 rounded-full'/></a>
                 <BsFacebook size={40} className='border-4 hover:border-indigo-800 rounded-full'/>
                 <BsInstagram size={40} className='border-4 hover:border-indigo-800 rounded-full'/>
