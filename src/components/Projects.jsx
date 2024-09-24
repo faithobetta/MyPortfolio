f22083e302e6ff0942f7c33af4557a105c766a04
faith obetta <obettafaith24@gmail.com>
2024-09-24 16:18:35 +0100

    project mapping

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,108 +1,69 @@
-import logo from "../assets/logo.png"
-import image from "../assets/image.png"
-import image2 from "../assets/image2.png"
-import image3 from "../assets/image3.png"
-import {FaGithub} from "react-icons/fa"
+import logo from "../assets/logo.png";
+import image from "../assets/image.png";
+import image2 from "../assets/image2.png";
+import image3 from "../assets/image3.png";
+import { FaGithub } from "react-icons/fa";
 
-const projects[
+const projects = [
     {
         id: 1,
-        image: "logo",
+        image: logo,
         title: "Makeup Match",
         demoLink: "http://make-up-match.shop",
         github: "https://github.com/faithobetta/MakeupMatch",
     },
     {
         id: 2,
-        image: "image",
+        image: image,
         title: "CatPals",
         demoLink: "https://cat-pals.vercel.app/",
         github: "https://github.com/faithobetta/CatPals",
     },
     {
         id: 3,
-        image: "image2",
+        image: image2,
         title: "Podcast",
-        demoLink: "podcastapp-psi.vercel.app",
+        demoLink: "https://podcastapp-psi.vercel.app/",
         github: "https://github.com/faithobetta/frontend-podcast",
     },
     {
         id: 4,
-        image: "image3",
+        image: image3,
         title: "EventFinder",
         demoLink: "https://event-finder-zeta.vercel.app/",
         github: "https://github.com/faithobetta/EventFinder",
     },
-   
-]
+];
 
-export default function Projects(){
-
-    return(
+export default function Projects() {
+    return (
         <>
-            <div className='bg-white m-10  max-w-full' id="projects">
+            <div className='bg-white m-10 max-w-full' id="projects">
                 <div className="grid justify-items-center m-10">
                     <h1 className="text-3xl mt-10">My Projects</h1>
                 </div>
 
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-5">
-                    <div className="max-w-md mx-auto rounded-xl shadow-2xl shadow-black/50 overflow-hidden md:max-w-2xl hover:animate-pulse">
-                        <div className="md:flex">
-                            <div className="p-16">
-                                <div className="md:shrink-0 p-3">
-                                    <img src={logo} alt="" width={150} height={10} />
+                    {projects.map((items) => (
+                        <div key={items.id} className="max-w-md mx-auto rounded-xl shadow-2xl shadow-black/50 overflow-hidden md:max-w-2xl hover:animate-pulse">
+                            <div className="md:flex">
+                                <div className="p-16">
+                                    <div className="md:shrink-0 p-3">
+                                        <img src={items.image} alt={items.title} width={150} height={10} />
+                                    </div>
+                                    <div className="flex flex-col items-center mt-4"> 
+                                        <a href={items.demoLink} className="uppercase tracking-wide  text-center">{items.title}</a>
+                                        <a href={items.github} rel="noopener noreferrer" className="text-4xl mt-2"> 
+                                            <FaGithub />
+                                        </a>
+                                    </div>
                                 </div>
-                                <a href="http://make-up-match.shop" className="uppercase tracking-wide p-6">Makeup Match</a>
-                                <a href="https://github.com/faithobetta/MakeupMatch" className="text-4xl"><FaGithub /></a>
                             </div>
                         </div>
-
-                    </div>
-
-                    <div className="max-w-md mx-auto rounded-xl shadow-2xl shadow-black/50 overflow-hidden md:max-w-2xl hover:animate-pulse">
-                        <div className="md:flex">
-                            <div className="p-16">
-                                <div className="md:shrink-0 p-5">
-                                    <img src={image} alt="" width={250} height={200} />
-                                </div>
-                                <a href="https://cat-pals.vercel.app/" className="uppercase tracking-wide p-2">CatPals</a>
-                                <a href="https://github.com/faithobetta/CatPals" className="text-4xl"><FaGithub /></a>
-                            </div>
-                        </div>
-
-                    </div>
-                    <div className="max-w-md mx-auto rounded-xl shadow-2xl shadow-black/50 overflow-hidden md:max-w-2xl hover:animate-pulse">
-                        <div className="md:flex">
-                            <div className="p-16">
-                                <div className="md:shrink-0 p-5">
-                                    <img src={image2} alt="" width={150} height={150} />
-                                </div>
-                                <a href="podcastapp-psi.vercel.app" className="uppercase tracking-wide p-6">Podcast</a>
-                                <a href="https://github.com/faithobetta/frontend-podcast" className="text-4xl"><FaGithub /></a>
-                            </div>
-                        </div>
-
-                    </div>
-                    <div className="max-w-md mx-auto rounded-xl shadow-2xl shadow-black/50 overflow-hidden md:max-w-2xl hover:animate-pulse">
-                        <div className="md:flex">
-                            <div className="p-16">
-                                <div className="md:shrink-0 p-5">
-                                    <img src={image3} alt="" width={150} height={150} />
-                                </div>
-                                <a href="https://event-finder-zeta.vercel.app/" className="uppercase tracking-wide p-6">EventFinder</a>
-                                <a href="https://github.com/faithobetta/EventFinder" className="text-4xl"><FaGithub /></a>
-                            </div>
-                        </div>
-
-                    </div>
+                    ))}
                 </div>
                 <hr />
-
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-5">
-
-                </div>
             </div>
         </>
-    )
+    );
 }
-
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,3 +1,17 @@
+const technology = [
+    { id: 1, name: "HTML & CSS", rating: 12/12 },
+    { id: 2, name: "Tailwind CSS", rating: 10/12 },
+    { id: 3, name: "Javascript", rating: 11/12 },
+    { id: 4, name: "React JS", rating: 11/12 },
+    { id: 5, name: "UI|UX", rating: 10/12 },
+    { id: 6, name: "figma", rating: 8/12 },
+    { id: 7, name: "Node JS", rating: 8/12 },
+    { id: 8, name: "Express JS", rating: 8/12 },
+    { id: 9, name: "MySQL", rating: 8/12 },
+    { id: 10, name: "MongoDB", rating: 8/12 },
+]
+
+
 const Skills = () => {
     return (
 
@@ -5,76 +19,15 @@ const Skills = () => {
         <div className="mt-12 lg:mb-32 text-white" id="skills">
             <div className="container m-auto px-4">
                 <h2 className="text-3xl sm:text-4xl font-bold text-center mb-8 sm:mb-12">skills</h2>
-                <div className="mt-14">
-                    <div className="flex justify-between">
-                        <h2 className="text-lg font-semibold">HTML & CSS</h2>
+                {technology.map((item) =>(
+                    <div key={(item.id)} className="mt-14">
+                        <div className="flex justify-between">
+                            <h2 className="text-lg font-semibold">{item.name}</h2>
+                        </div>
+                        <div className="w-full h-4 bg-slate-600 block rounded-md" style={{ width: `${item.rating * 100}%` }}></div>
                     </div>
-                    <div className="w-full h-4 bg-slate-600 block rounded-md"></div>
-                </div>
-
-                <div className="mt-6">
-                    <div className="flex justify-between">
-                        <h2 className="text-lg font-semibold">Tailwind CSS</h2>
-                    </div>
-                    <div className="w-full  h-4 bg-slate-600 block rounded-md"></div>
-                </div>
-
-                <div className="mt-6">
-                    <div className="flex justify-between">
-                        <h2 className="text-lg font-semibold">React JS</h2>
-                    </div>
-                    <div className="w-full  h-4 bg-slate-600 block rounded-md"></div>
-                </div>
-
-                <div className="mt-6">
-                    <div className="flex justify-between">
-                        <h2 className="text-lg font-semibold">Javascript</h2>
-                    </div>
-                    <div className="w-full  h-4 bg-slate-600 block rounded-md"></div>
-                </div>
-
-                <div className="mt-6">
-                    <div className="flex justify-between">
-                        <h2 className="text-lg font-semibold">UI/UX</h2>
-                    </div>
-                    <div className="w-10/12  h-4 bg-slate-600 block rounded-md"></div>
-                </div>
-
-                <div className="mt-6">
-                    <div className="flex justify-between">
-                        <h2 className="text-lg font-semibold">Figma</h2>
-                    </div>
-                    <div className="w-10/12  h-4 bg-slate-600 block rounded-md"></div>
-                </div> 
-
-                <div className="mt-6">
-                    <div className="flex justify-between">
-                        <h2 className="text-lg font-semibold">Node JS</h2>
-                    </div>
-                    <div className="w-8/12  h-4 bg-slate-600 block rounded-md"></div>
-                </div>
-
-                <div className="mt-6">
-                    <div className="flex justify-between">
-                        <h2 className="text-lg font-semibold">Express JS</h2>
-                    </div>
-                    <div className="w-8/12  h-4 bg-slate-600 block rounded-md"></div>
-                </div>
-
-                <div className="mt-6">
-                    <div className="flex justify-between">
-                        <h2 className="text-lg font-semibold">MongoDB</h2>
-                    </div>
-                    <div className="w-8/12  h-4 bg-slate-600 block rounded-md"></div>
-                </div>
-
-                <div className="mt-6">
-                    <div className="flex justify-between">
-                        <h2 className="text-lg font-semibold">MySQL</h2>
-                    </div>
-                    <div className="w-8/12  h-4 bg-slate-600 block rounded-md"></div>
-                </div>
-
+                ))}
+                
             </div>
 
         </div>
